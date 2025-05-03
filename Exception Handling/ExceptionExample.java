b3cf8fb34b6fcf391f6f4badae73f72251bc1e5b
anibalguijarro <guijarroa01@gmail.com>
2025-05-03 17:08:55 -0600

    Added more examples of Exception Handling

---
+++ Exception Handling/ExceptionExample.java
@@ -0,0 +1,31 @@
+public class ExceptionExample {
+    public static void main(String[] args){
+
+        // Example 1 -> Try-Catch using 'NumberFormatException'
+        try {
+
+            // Code that may cause an exception
+            int number = Integer.parseInt("Word");
+
+        } catch (NumberFormatException nfe) {
+
+            // Code that we want to execute if this type of exception happens
+            System.out.println("Error! Can't make an int out of that.");
+
+        }
+        System.out.println("End of example 1\n");
+
+
+        // Example 2 -> Using an independent method and catching a generic 'Exception'
+        try {
+            getInt();
+        } catch (Exception e) {
+            System.out.println("Error from method getInt()!!");
+        }
+
+    }
+
+    private static void getInt() {
+        int myInt = Integer.parseInt("pants");
+    }
+}
