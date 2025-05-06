08f70b23940c0c9d33103dc26620017da9cb0d2b
anibalguijarro <guijarroa01@gmail.com>
2025-05-05 20:32:16 -0600

    Add the Student class as a child class of Person

--- Inheritance/Student.java
+++ Inheritance/Student.java
@@ -1,3 +1,19 @@
-public class Student{
-    
+public class Student extends Person{
+    private int studentID;
+    private float grade;
+
+    public Student(String name, String lastname, int age, int studentID, float grade){
+        super(name, lastname, age);
+        this.studentID = studentID;
+        this.grade = grade;
+    }
+
+    public void showData(){
+        System.out.println("Name: " + getName() 
+            + "\nLastname: " + getLastname()
+            + "\nAge: " + getAge()
+            + "\nID: " + studentID
+            + "\nGrade: " + grade);
+    }
+
 }
