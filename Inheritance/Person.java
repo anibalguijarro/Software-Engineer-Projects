87ccda732a28ce60b10a456b1a5bf183e763d1c7
anibalguijarro <guijarroa01@gmail.com>
2025-05-05 20:18:21 -0600

    Add class Person with contructor and getters

---
+++ Inheritance/Person.java
@@ -0,0 +1,24 @@
+public class Person{
+    private String name;
+    private String lastname;
+    private int age;
+
+    public Person(String name, String lastname, int age){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}
