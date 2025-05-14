c09a449198bfd9d326350818ad6e6f87ba92ff9a
anibalguijarro <guijarroa01@gmail.com>
2025-05-13 18:24:21 -0600

    Add abstract class (LivingBeing) and a child class (Plant)

---
+++ Abstraction/LivingBeing.java
@@ -0,0 +1,8 @@
+/*
+ * >> Abstract class can't be instantiated
+ */
+public abstract class LivingBeing{
+
+    public abstract void feed();
+
+}
---
+++ Abstraction/Plant.java
@@ -0,0 +1,8 @@
+public class Plant extends LivingBeing{
+
+    @Override
+    public void feed(){
+        System.out.println("Plants feed through photosynthesis");
+    }
+
+}
